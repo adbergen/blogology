5600c21ff77c9774313f4d894cabdfae92c95d1a
Anthony Bergen <anthonydbergen@gmail.com>
2021-03-06 11:51:52 -0500

    added styled divider below Whats Happening

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,33 +1,36 @@
 <template>
   <q-page>
-    <div class="q-py-lg q-px-md">
-      <q-input
-        bottom-slots
-        v-model="newContent"
-        label="What's Happening?"
-        counter
-        autogrow
-        maxlength="280"
-        class="new-content"
-      >
-        <template v-slot:before>
-          <q-avatar size="xl">
-            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
-          </q-avatar>
-        </template>
-
-        <template v-slot:after>
-          <q-btn
-            unelevated
-            rounded
-            no-caps
-            :disable="!newContent"
-            color="primary"
-            label="Publicize"
-          />
-        </template>
-      </q-input>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newContent"
+          label="What's Happening?"
+          counter
+          autogrow
+          maxlength="280"
+          class="new-content"
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          class="q-mb-lg"
+          unelevated
+          rounded
+          no-caps
+          :disable="!newContent"
+          color="primary"
+          label="Publicize"
+        />
+      </div>
     </div>
+    <q-separator class="divider" size="10px" color="grey-2" />
   </q-page>
 </template>
 
@@ -47,4 +50,8 @@ export default {
   textarea
     font-size: 19px
     line-height: 1.4 !important
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
 </style>
