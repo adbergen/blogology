aa9ce7dfa1cf8fe0f0d7aa0f624859c3319794e5
Anthony Bergen <anthonydbergen@gmail.com>
2021-03-06 00:25:27 -0500

    added about section with styling and filler text

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <q-page> <h5>About</h5> </q-page>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Blogology</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque quos
+        modi accusantium repellendus maxime porro unde nulla aut! Veniam
+        consequuntur eveniet iusto incidunt obcaecati reprehenderit esse aut et
+        maiores. A.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque quos
+        modi accusantium repellendus maxime porro unde nulla aut! Veniam
+        consequuntur eveniet iusto incidunt obcaecati reprehenderit esse aut et
+        maiores. A.
+      </p>
+    </div>
+  </q-page>
 </template>
 
 <script>
