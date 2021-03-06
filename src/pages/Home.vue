cc8e9569be591f3ac7ba6b8b624586b5cf448ed1
Anthony Bergen <anthonydbergen@gmail.com>
2021-03-06 00:44:32 -0500

    added What's happening section to Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,50 @@
 <template>
-  <q-page> <h5>Home</h5> </q-page>
+  <q-page>
+    <div class="q-py-lg q-px-md">
+      <q-input
+        bottom-slots
+        v-model="newContent"
+        label="What's Happening?"
+        counter
+        autogrow
+        maxlength="280"
+        class="new-content"
+      >
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+          </q-avatar>
+        </template>
+
+        <template v-slot:after>
+          <q-btn
+            unelevated
+            rounded
+            no-caps
+            :disable="!newContent"
+            color="primary"
+            label="Publicize"
+          />
+        </template>
+      </q-input>
+    </div>
+  </q-page>
 </template>
 
 <script>
 export default {
-  name: "PageIndex"
+  name: "PageIndex",
+  data() {
+    return {
+      newContent: ""
+    };
+  }
 };
 </script>
+
+<style lang="sass">
+.new-content
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+</style>
