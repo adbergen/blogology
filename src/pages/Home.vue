8c2b1a3ff18aeda256ec85adc1d70a3b817ee29d
Anthony Bergen <anthonydbergen@gmail.com>
2021-03-06 12:23:38 -0500

    added post content with styling

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,16 +4,18 @@
       <div class="col">
         <q-input
           bottom-slots
-          v-model="newContent"
+          v-model="newPost"
           label="What's Happening?"
           counter
           autogrow
           maxlength="280"
-          class="new-content"
+          class="new-post"
         >
           <template v-slot:before>
             <q-avatar size="xl">
-              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+              <img
+                src="https://en.gravatar.com/userimage/202821422/a87e067304889e88293c27db83cba1e3.jpeg"
+              />
             </q-avatar>
           </template>
         </q-input>
@@ -24,13 +26,47 @@
           unelevated
           rounded
           no-caps
-          :disable="!newContent"
+          :disable="!newPost"
           color="primary"
           label="Publicize"
         />
       </div>
     </div>
     <q-separator class="divider" size="10px" color="grey-2" />
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+            <img
+              src="https://en.gravatar.com/userimage/202821422/a87e067304889e88293c27db83cba1e3.jpeg"
+            />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>Anthony Bergen</strong>
+            <span class="text-grey-7"> @bergenphysique</span>
+          </q-item-label>
+          <q-item-label class="post-content text-body1"
+            >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea
+            distinctio dolore dolorem? Nostrum explicabo deserunt repudiandae,
+            id earum pariatur. Eum aliquid autem modi architecto in labore
+            dignissimos id quod? Incidunt!
+          </q-item-label>
+          <div class="row justify-between q-mt-sm post-icons">
+            <q-btn flat round color="grey" size="sm" icon="far fa-comment" />
+            <q-btn flat round color="grey" size="sm" icon="fas fa-retweet" />
+            <q-btn flat round color="grey" size="sm" icon="far fa-heart" />
+            <q-btn flat round color="grey" size="sm" icon="fas fa-trash" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -39,14 +75,14 @@ export default {
   name: "PageIndex",
   data() {
     return {
-      newContent: ""
+      newPost: ""
     };
   }
 };
 </script>
 
 <style lang="sass">
-.new-content
+.new-post
   textarea
     font-size: 19px
     line-height: 1.4 !important
@@ -54,4 +90,8 @@ export default {
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
+.post-content
+  white-space: pre-line
+.post-icons
+  margin-left: -5px
 </style>
