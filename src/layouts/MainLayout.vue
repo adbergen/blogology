6487909f831e1dbe06ebc69eab8597d17c4fa20a
Anthony Bergen <anthonydbergen@gmail.com>
2021-03-05 23:22:51 -0500

    added home and about section with routes and icons to left side bar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,8 +10,27 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
+    <q-drawer show-if-above v-model="left" side="left" :width="283" bordered>
       <q-icon class="q-pa-md" name="fas fa-dna" size="lg" color="primary" />
+
+      <q-list>
+        <q-item to="/" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+        <q-item to="/about" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="psychology" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold"
+            >About</q-item-section
+          >
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="right" side="right" bordered>
